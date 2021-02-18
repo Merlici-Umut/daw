88aaade4611d3a2bafa6119d4d253f260f9521fb
Merlici-Umut <umut.merlici@elev.cihcahul.md>
2021-02-18 13:13:32 +0200

    Comit2

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('title','Slide show')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="owl-carousel owl-theme">
+                    <div class="item"><h4>1</h4></div>
+                    <div class="item"><h4>2</h4></div>
+                    <div class="item"><h4>3</h4></div>
+                    <div class="item"><h4>4</h4></div>
+                    <div class="item"><h4>5</h4></div>
+                    <div class="item"><h4>6</h4></div>
+                    <div class="item"><h4>7</h4></div>
+                    <div class="item"><h4>8</h4></div>
+                    <div class="item"><h4>9</h4></div>
+                    <div class="item"><h4>10</h4></div>
+                    <div class="item"><h4>11</h4></div>
+                    <div class="item"><h4>12</h4></div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
